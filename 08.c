944b79434fccb35421162d40d11bcff739233593
Caley R <crombout@my.bcit.ca>
2025-02-09 10:55:17 -0800

    Exercises & Task 8

---
+++ 08.c
@@ -0,0 +1,11 @@
+float *(*p)[4];
+// p is a pointer to an array of 4 pointers to float.
+
+double *p[4];
+// p is an array of 4 pointers to double.
+
+int *(p[4]);
+// p is a pointer to an array of 4 integers.
+
+void *(p[4]);
+// p is an array of 4 pointers to void.
---
+++ Exercises.c
@@ -0,0 +1,8 @@
+// digits is an array of 10 integers
+int digits[10];
+
+// mat is an array of three arrays of five integers
+int mat[3][5];
+
+// psa is an array of 10 pointers to char.
+char *psa[10];
